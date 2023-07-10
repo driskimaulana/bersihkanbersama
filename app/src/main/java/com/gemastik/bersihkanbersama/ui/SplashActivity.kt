fc4992f4fea1b5803e7a000fa1a697f7cc126e8a
D'Riski Maulana <maulanadriski@gmail.com>
2023-07-10 22:51:44 +0700

    minor

--- app/src/main/java/com/gemastik/bersihkanbersama/ui/SplashActivity.kt
+++ app/src/main/java/com/gemastik/bersihkanbersama/ui/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gemastik.bersihkanbersama.R
 import com.gemastik.bersihkanbersama.ui.main.MainActivity
+import com.gemastik.bersihkanbersama.ui.onboarding.OnBoardingActivity
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -16,7 +17,8 @@ class SplashActivity : AppCompatActivity() {
 
         GlobalScope.launch {
             delay(3000)
-            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+//            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            startActivity(Intent(this@SplashActivity, OnBoardingActivity::class.java))
             finish()
         }
     }
